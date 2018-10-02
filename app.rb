369ac7f90f0d9a0c4961db2ae9c5c0fc60ba0e3f
Leo <kl@alushta.net>
2018-10-02 15:33:07 +0300

    Save comments to @comments variable

--- app.rb
+++ app.rb
@@ -21,12 +21,13 @@ configure do
 		created_date DATE,
 		content TEXT
 	)'
-	# @db.execute 'CREATE TABLE IF NOT EXISTS Messages
-	# (
-	# 	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	# 	created_date DATE,
-	# 	message TEXT
-	# )'
+	@db.execute 'CREATE TABLE IF NOT EXISTS Comments
+	(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		created_date DATE,
+		content TEXT,
+		post_id INTEGER
+	)'
 
 end
 
@@ -57,6 +58,8 @@ get '/details/:post_id' do
 	results = @db.execute 'select * from Posts where id = ?', [post_id]
 	@row = results[0]
 
+	@comments = @db.execute 'select * from Comments where post_id = ? order by id', [content]
+
 	erb :details
 end
 
@@ -64,5 +67,9 @@ post '/details/:post_id' do
 	post_id = params[:post_id]
 
 	content = params[:content]
+
+	@db.execute 'insert into Comments (content, created_date, post_id) values (?, datetime(), ?)', [content, post_id]
+
+	redirect to('/details/' + post_id)
 
 end
