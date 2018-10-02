bf083e8678da964b335bc41d898a272e845d8e17
Leo <kl@alushta.net>
2018-10-02 13:55:10 +0300

    Add /details view

--- app.rb
+++ app.rb
@@ -44,6 +44,12 @@ post '/new' do
 	redirect to '/'
 end
 
-get '/details/:id' do
+get '/details/:post_id' do
+	post_id = params[:post_id]
 
+	results = @db.execute 'select * from Posts where id = ?', [post_id]
+	@row = results[0]
+
+	erb :details
 end
+
